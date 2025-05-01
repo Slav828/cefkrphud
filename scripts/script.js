79b9c736f83117976e95ceb55393c52036ad8a80
Slav828 <fangerher@gmail.com>
2025-05-02 00:13:48 +0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+let health = document.querySelector('.playerInfo__params__heal__progress');
+let armour = document.querySelector('.playerInfo__params__armor__progress');
+let breath = document.querySelector(".playerInfo__params__eat__progress");
+let pmoney = document.querySelector(".moneyCount");
+let gcnt = document.querySelector(".gunCount");
+let gcmnt = document.querySelector(".gunCountMax");
+let gun = document.querySelector(".gunUse");
+
+
+
+cef.on("game:hud:newVisibleState", (success) => {
+	cef.hide(!success);
+});
+
+cef.emit("game:data:pollPlayerStats", true, 50);
+cef.on("game:data:playerStats", (hp, max_hp, arm, breath, wanted, weapon, ammo, max_ammo, money, speed) => {
+    health.style.width = hp + '%';
+    health.style.radius = max_hp + '%';
+    armour.style.width = arm + '%';
+    breath.style.width = breath + '%';
+    gcnt.style.textContent = ammo;
+    gcmnt.style.textContent = max_ammo;
+    pmoney.textContent = money.toLocaleString('de-DE');
+
+    gun.src = "/image/guns/" + weapon + ".png";
+
+    if(wanted ==0){
+        for(let i = 1;i<=6;++i){
+            let star = document.querySelector(`.star${i}`);
+            star.src = "./image/star-none.png";
+        }
+    } else{
+        for(let i = 1;i<=wanted;++i){
+            let star = document.querySelector(`.star${i}`);
+            star.src = "./image/star.png";
+        }
+    }
+    
+    
+});
